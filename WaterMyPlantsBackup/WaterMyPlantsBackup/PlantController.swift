f079e1fcb2aa9a42147de985f191f6d0866ddca6
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-04 22:30:58 -0800

    added model controller files

---
+++ WaterMyPlantsBackup/WaterMyPlantsBackup/PlantController.swift
@@ -0,0 +1,42 @@
+//
+//  PlantController.swift
+//  WaterMyPlantsBackup
+//
+//  Created by Jorge Alvarez on 2/4/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+let baseURL = URL(string: "")! // enter backend url or firebase url here
+
+class PlantController {
+    
+    typealias CompletionHandler = (Error?) -> Void
+    
+    init() {
+        fetchPlantsFromServer()
+    }
+    
+    func fetchPlantsFromServer(completion: @escaping CompletionHandler = { _ in }) {
+        print("fetchPlantsFromServer")
+        //let requestURL = baseURL.appendingPathExtension("json")
+    }
+    
+    private func updatePlants(with representations: [PlantRepresentation]) throws {
+        print("updatePlants with representations")
+    }
+    
+    private func update(plant: Plant, with representation: PlantRepresentation) {
+        print("update plant with representation")
+    }
+    
+    func sendPlantsToServer(plant: Plant, completion: @escaping CompletionHandler = { _ in }) {
+        print("sendPlantsToServer")
+    }
+    
+    func deletePlantFromServer(plant: Plant, completion: @escaping CompletionHandler = { _ in }) {
+        print("deletePlantFromServer")
+    }
+}
---
+++ WaterMyPlantsBackup/WaterMyPlantsBackup/UserController.swift
@@ -0,0 +1,15 @@
+//
+//  UserController.swift
+//  WaterMyPlantsBackup
+//
+//  Created by Jorge Alvarez on 2/4/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class UserController {
+    
+}
+
