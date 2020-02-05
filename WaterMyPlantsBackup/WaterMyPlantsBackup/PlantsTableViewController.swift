965075a0d05d41b2ea24129aeb9c02253fa746f8
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-04 20:14:34 -0800

    finished detail storyboard

--- WaterMyPlantsBackup/WaterMyPlantsBackup/PlantsTableViewController.swift
+++ WaterMyPlantsBackup/WaterMyPlantsBackup/PlantsTableViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
-
+// PlantCell
+// AddPlantSegue
+// DetailPlantSegue
+// LoginSegue ?
 class PlantsTableViewController: UITableViewController {
+    
+    var dummyArray: [String] = ["Plant 1", "Plant 2", "Plant 3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,24 +28,22 @@ class PlantsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return dummyArray.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlantCell", for: indexPath)
 
         // Configure the cell...
-
+        let testCell = dummyArray[indexPath.row]
+        cell.textLabel?.text = testCell
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -50,7 +53,6 @@ class PlantsTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -60,7 +62,6 @@ class PlantsTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
