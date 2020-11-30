ae0b30858b3821835a7cfc2a0a11c8c65c44f2ca
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-11-30 15:36:54 -0800

    changes datepicker style to work with 14.2, makes day selector have default background

--- Remindew/Remindew/PlantsTableViewController.swift
+++ Remindew/Remindew/PlantsTableViewController.swift
@@ -43,6 +43,7 @@ import AVFoundation
 // TODO: only scale down images if they need it, and at an acceptable size
 // TODO: clean up image and watering logic. remove old debugging prints
 // TODO: rename refactor userController with plantController
+// TODO: touch outside textfield dismisses keyboard?
 
 class PlantsTableViewController: UITableViewController {
     
